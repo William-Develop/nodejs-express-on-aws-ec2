452f931769b4afec9a38a4f7cfccc1c48dcce02c
✨ William Maldonado ✨ <willpaiolo@live.com>
2024-01-09 17:28:50 -0600

    remove space

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,7 +3,7 @@
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node 10
+nvm install 10
 
 #create the working directory if it doesen't exist.
 DIR="/home/ec2-user/express-app"
