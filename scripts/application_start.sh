71ef55b450296754f20663a2d0a996dfc5a4ebd1
✨ William Maldonado ✨ <willpaiolo@live.com>
2024-01-08 23:08:53 -0600

    add pm2

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,12 +4,21 @@ sudo chmod -R 777 /home/ec2-user/express-app
 #navigate into working directory
 cd /home/ec2-user/express-app
 
-#add npm and node to path
+# add npm and node to path
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" #loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" #loads nvm bash completion
 
 npm install
 
-# start the app
-node app.js > app.out.log 2> app.err.log < /dev/null &
+# install pm2
+npm install pm2 -g
+
+# start the app with PM2
+pm2 start app.js --name "my-app"
+
+# save the current process list for restarts
+pm2 save
+
+# make pm2 auto-start on boot
+pm2 startup
