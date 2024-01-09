7930671be080b7ad4f28f3c6cc06d8b65968aef9
✨ William Maldonado ✨ <willpaiolo@live.com>
2024-01-09 13:47:17 -0600

    reverted code

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,28 +1,10 @@
-# #!/bin/bash
-
-# #download node and npm
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-# . ~/.nvm/nvm.sh
-# nvm install node
-
-# #create the working directory if it doesen't exist.
-# DIR="/home/ec2-user/express-app"
-# if [ -d "$DIR" ]; then
-#     echo "${DIR} exists"
-# else
-#     echo "Creating ${DIR} directory"
-#     mkdir ${DIR}
-# fi    
------------------------------------------------------------------------
-# Modified code to install npm
-
 #!/bin/bash
+
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
-#install npm
-npm install -g npm
+
 #create the working directory if it doesen't exist.
 DIR="/home/ec2-user/express-app"
 if [ -d "$DIR" ]; then
@@ -30,4 +12,4 @@ if [ -d "$DIR" ]; then
 else
     echo "Creating ${DIR} directory"
     mkdir ${DIR}
-fi
+fi    
