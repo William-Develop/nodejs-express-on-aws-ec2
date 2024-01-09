373984b785fbad283b1ee402730121fe69933691
✨ William Maldonado ✨ <willpaiolo@live.com>
2024-01-09 15:32:45 -0600

    add pm2

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 #Stopping existing node servers
+# echo "Stopping any existing node servers"
+# pkill node
+# ----------------------------------------------
+#!/bin/bash
+#Stopping existing node servers
 echo "Stopping any existing node servers"
-pkill node
+cd /home/ec2-user/express-app
+./node_modules/pm2/bin/pm2 stop app.js || true
+./node_modules/pm2/bin/pm2 delete app.js || true
