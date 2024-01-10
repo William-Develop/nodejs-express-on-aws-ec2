30709adeec503bc7723f1f27910ae25a92ee1c79
✨ William Maldonado ✨ <willpaiolo@live.com>
2024-01-09 21:55:12 -0600

    add stop pm2

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -23,5 +23,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" #loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" #loads nvm bash completion
 npm install
+
+# stop the app with pm2
+./node_modules/pm2/bin/pm2 stop app.js
 # start the app with pm2
 ./node_modules/pm2/bin/pm2 start app.js
