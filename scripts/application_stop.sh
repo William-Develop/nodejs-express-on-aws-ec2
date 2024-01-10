74f2a0c6ddfeb894329e4160286221e137ea6e35
✨ William Maldonado ✨ <willpaiolo@live.com>
2024-01-10 12:09:23 -0600

    remove unused code

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 #Stopping existing node servers
-# echo "Stopping any existing node servers"
-# pkill node
-# ----------------------------------------------
-#!/bin/bash
-#Stopping existing node servers
 echo "Stopping any existing node servers"
 cd /home/ec2-user/express-app
 ./node_modules/pm2/bin/pm2 stop app.js || true
